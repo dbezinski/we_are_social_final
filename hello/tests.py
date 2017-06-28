b87622c6a4ed297cf4cf8f9b15da0e5e1bd19a98
Dimitar Bezinski <d.bezinski@gmail.com>
2017-06-28 19:34:32 +0100

    Test Added

--- hello/tests.py
+++ hello/tests.py
@@ -1,6 +1,9 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.test import TestCase
+from hello.views import get_index
+from django.core.urlresolvers import resolve
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_home_page_resolves(self):
+        home_page = resolve('/')
+        self.assertEqual(home_page.func, get_index)
