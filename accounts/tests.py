a0c38959ea300812a38582eb5216228d866fbbe9
Dimitar Bezinski <d.bezinski@gmail.com>
2017-06-28 19:36:52 +0100

    Test Added

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,9 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from unittest import TestCase
 
-from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_adding_something_simple(self):
+        self.assertEqual(1 + 2, 3)
+
+    def test_adding_something_isnt_equal(self):
+        self.assertNotEqual(1 + 1, 3)  # changed to make the assertNotEqual test fail
