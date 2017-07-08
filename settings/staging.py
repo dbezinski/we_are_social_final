0d01ebb287b0f1d214ac0b160a5ae0d83eed9171
Dimitar Bezinski <d.bezinski@gmail.com>
2017-07-08 22:20:10 +0100

    Initial Commit

--- settings/staging.py
+++ settings/staging.py
@@ -1,4 +1,5 @@
 from base import *
+import dj_database_url
 
 DEBUG = True
 
@@ -8,6 +9,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+DATABASES['default'] = dj_database_url.config("CLEARDB_DATABASE_URL")
+
 
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_lRJ210v4A3Z1wdwpi5469nZj')
